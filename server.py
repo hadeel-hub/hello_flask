87433808578e68d8954250f1064d80b178b565e1
hadeel-hub <hadeela_k2050@yahoo.com>
2022-09-18 11:39:04 +0300

    playground

---
+++ server.py
@@ -0,0 +1,17 @@
+# from turtle import color
+from flask import Flask, render_template 
+# from hello import repeat 
+app = Flask(__name__)
+
+@app.route("/play/<int:x>/<color>")
+def play(x,color):
+    # repeat(x)
+    return render_template("index.html",x = x, color =color)
+
+
+@app.route("/")
+def welcome():
+    return "Welcome to Flask"
+
+if __name__ == '__main__':
+    app.run(debug=True)
